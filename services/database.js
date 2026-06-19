ffd66623b629196a0bed30c2f18412ed310ad677
new user <newuser@Rey-MacBook-Pro.routerlogin.net>
2026-06-19 16:11:04 -0400

    fix ssl database connection

--- services/database.js
+++ services/database.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: { rejectUnauthorized: false }
 });
 
 const query = (text, params) => db.query(text, params);
